e59b08d4fc3a6d8d05ac1bac4130b17d0e44919e
Jordan Page <jp0046@mix.wvu.edu>
2020-09-27 20:16:52 -0400

    Developed Database and created login/logout features

---
+++ about.php
@@ -0,0 +1,103 @@
+<?php
+require 'includes/header.php';
+?>
+
+<main>
+<link rel="stylesheet" href="css/about.css">
+
+    <div class="about">
+        <h1> About Me, Jordan Page!</h1>
+        <h2>Contact:<br> </h2>
+        <h3>304-906-5239 jpage58@gmail.com</h3>
+    </div>
+
+    <div class="row">
+        <div class="column">
+            <div class="card">
+                <img src="images/selfie.jpg">
+                <div class="container">
+                    <p>A Portrait of Me!</p>
+                </div>
+
+            </div>
+        </div>
+
+        <div class="column">
+            <div class="card">
+                <img src="images/baseball.jpg">
+                <div class="container">
+                    <p>Playing Baseball!</p>
+                </div>
+
+            </div>
+        </div>
+
+
+
+    </div>
+
+    <div class="paragraph">
+        <p>Hello, my name is Jordan Page and I am a junior-level computer engineering student at West Virginia
+            University.
+            I am currently learning to code in HTML,CSS, and PHP in my software engineering laboratory class.
+            Listed below are some of my favorite pastimes/hobbies!
+        </p>
+
+    </div>
+
+    <div class="row">
+        <div class="column">
+            <div class="card">
+                <img src="images/dumbell.jpg">
+                <div class="container">
+                    <p>I enjoy fitness and try to exercise <br>everyday.</p>
+                </div>
+
+            </div>
+        </div>
+
+        <div class="column">
+            <div class="card">
+                <img src="images/friends.jpg">
+                <div class="container">
+                    <p>I love spending time with my family, friends, <br>and my girlfriend!</p>
+                </div>
+
+            </div>
+        </div>
+        <div class="column">
+            <div class="card">
+                <img src="images/gaming.jpg">
+                <div class="container">
+                    <p>One way that I pass the time is to play<br> video games, either on my PC or my Xbox!</p>
+                </div>
+
+            </div>
+        </div>
+
+        <div class="column">
+            <div class="card">
+                <img src="images/buffalo.jpg">
+                <div class="container">
+                    <p>On the weekends I wait tables at Buffalo <br>Wild Wings and have been for 3 years! </p>
+                </div>
+
+            </div>
+        </div>
+
+        <div class="row">
+            <div class="column">
+                <div class="button">
+                    <p style="color: white;"> <a href="https://twitter.com/Jordan_Page21">Twitter</a> </p>
+                </div>
+            </div>
+
+
+
+            <div class="column">
+                <div class="button">
+                    <p style="color: white;"> <a href="https://www.facebook.com/jordan.page.395">Facebook</a> </p>
+                </div>
+            </div>
+        </div>
+</main>
